710db93f838f4f6a303e16849c1d41281626ca75
Mohamad Hamdo Alesmaeil <mohamad.h.alesmeil@gmail.com>
2018-07-21 12:28:06 +0200

    JPA interface repository has been implemented and now it is time for JPA annotations

--- src/main/java/de/hsba/a16/bi/mitfahrtszentrale/trip/TripRepository.java
+++ src/main/java/de/hsba/a16/bi/mitfahrtszentrale/trip/TripRepository.java
@@ -1,5 +1,6 @@
 package de.hsba.a16.bi.mitfahrtszentrale.trip;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.GeneratedValue;
@@ -9,27 +10,5 @@ import java.util.Map;
 import java.util.TreeMap;
 
 @Repository
-public class TripRepository {
-  private Map<Long, Trip> store= new TreeMap<>();
-
-  private long id;
-  private synchronized Long nextId (){
-      return ++id;
-  }
-
-  public Trip saveTrip (Trip trip){
-      Long id = nextId();
-      trip.setId(id);
-      store.put(id, trip);
-      return trip;
-  }
-  public Collection<Trip> findAll (){
-      return store.values();
-  }
-  public Trip findeById (Long id){
-      return store.get(id);
-  }
-    public Trip deleteById (Long id){
-        return store.remove(id);
-    }
+public interface TripRepository extends JpaRepository<Trip, Long> {
 }
